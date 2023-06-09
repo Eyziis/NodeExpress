42d476ffb75542b7f282b342aa32585a0a344ed0
k.bastin <kevin.bastin@outlook.com>
2023-06-09 15:23:16 +0200

    commit bureau

--- routes/locataires.js
+++ routes/locataires.js
@@ -3,40 +3,78 @@ var express = require('express');
 var router = express.Router();
 
 const {Locataire} = require("../models");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/', async function (req, res, next) {
-    var Result = await Locataire.findAll();
-    res.send(Result);
-
+    var Result = Locataire.findAll();
+    Result.then((r) => {
+        res.status(200).json(r);
+    }).catch((err) => {
+        res.status(500).json(err);
+    })
 });
 
-router.get('/test', function (req, res, next) {
-    Locataire.create({
+router.get('/mock', function (req, res, next) {
+    Locataire.bulkCreate([{
         nom: 'Frodon Sacquet',
         age: '38',
         email: "frodon@DeLaComté.unique",
         date_e: '1997-06-08'
-    })
-    Locataire.create({
+    }, {
         nom: 'Tanjiro Kamado',
         age: '16',
         email: "antidemon@nezuko.pillier",
         date_e: '2019-06-08'
+    }]).then((r) => {
+
+    }).catch((error) => {
+        res.status(500);
     })
+
+
     res.send('ok');
 });
 
-router.get('/:locataireId', async function (req, res, next) {
-    var Result = Locataire.findByPk(req.params.locataireId);
-    Result.then((r) => {
-        //res.send(200,r);
-        res.status(200).json(r);
-    })
+router.get('/search', async function (req, res, next) {
+    var Result;
+    if (req.query.id) {
+        var Result = Locataire.findByPk(req.query.id);
+        Result.then((r) => {
+            res.status(200).json(r);
+        })
+    } else if (req.query.nom && req.query.type) {
+
+        if (req.params.type === "Contient") {
+            Result = Locataire.findAll({where: {nom: {[Op.like]: '%' + req.query.nom + '%'}}})
+        } else {
+            Result = Locataire.findAll({where: {nom: {[Op.like]: req.query.nom + '%'}}})
+        }
+
+        Result.then((r) => {
+            res.status(200).json(r);
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
+    } else {
+        if (req.query.nom === '') {
+            Result = Locataire.findAll()
+            Result.then((r) => {
+                res.status(200).json(r);
+            })
+        } else {
+            res.status(400);
+        }
+    }
+
+
 });
 router.delete('/:locataireId', async function (req, res, next) {
-    var Result = Locataire.destroy({where: { id:req.params.locataireId}});
+    var Result = Locataire.destroy({where: {id: req.params.locataireId}});
     Result.then((r) => {
-        res.status(200);
+        res.status(200).json('locataire supprimé');
+    }).catch((err) => {
+        res.status(500).json(err);
     })
 });
 
@@ -44,7 +82,7 @@ router.put('/', async function (req, res, next) {
     var Result = Locataire.update(req.body, {where: {id: req.body.id}})
     Result.then((r) => {
         res.status(200).json(req.body);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(500).json(err);
     })
 });
@@ -52,13 +90,10 @@ router.put('/', async function (req, res, next) {
 router.post('/', async function (req, res, next) {
     var loc = req.body;
     delete loc.id;
-
     var Result = Locataire.create(loc)
     Result.then((r) => {
         res.send(r.dataValues);
     })
-
-
 });
 
 module.exports = router;
